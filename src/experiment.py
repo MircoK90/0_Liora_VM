6b8a6cbf9aebfb5d682e38ec415f04a45eb39331
Ubuntu <ubuntu@ip-172-31-47-211.eu-west-1.compute.internal>
2026-03-16 13:03:17 +0000

    schlecht_erklaert

--- src/experiment.py
+++ src/experiment.py
@@ -12,7 +12,7 @@ mlflow.set_tracking_uri("http://127.0.0.1:8080")
 
 # Define experiment name, run name and artifact_path name
 apple_experiment = mlflow.set_experiment("Apple_Models")
-run_name = "rist_run"
+run_name = "first_run"
 artifact_path = "rf_apples"
 
 # Import Database
@@ -31,7 +31,7 @@ params = {
     "random_state": 42,
 }
 rf = RandomForestRegressor(**params)
-rf.fit(X_train, y_train)b
+rf.fit(X_train, y_train)
 
 # Evaluate model
 y_pred = rf.predict(X_val)
